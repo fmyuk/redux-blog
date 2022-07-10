201f0a8c6e84725e408158fc49d3c4ec1606269f
fumi uk <dcfumiya.238@gmail.com>
2022-07-10 11:05:13 +0900

    redux-blog

--- src/components/markdown/MarkdownEditor.js
+++ src/components/markdown/MarkdownEditor.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useMemo } from "react";
 import SimpleMde from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { marked } from "marked";
+import highlightjs from "highlight.js";
+import "highlight.js/styles/github.css";
+import { storage } from "../../config/firebase";
 
 const MarkdownEditor = ({ description, setDescription }) => {
+
+  marked.setOptions({
+    highlight: (code, lang) => {
+      return highlightjs.highlightAuto(code, [lang]).value;
+    },
+  });
+  
+  const imageUploadFunction = (file) => {
+    const storageRef = storage.ref(`images`);
+    const imagesRef = storageRef.child(file.name);
+    const upLoadTask = imagesRef.put(file);
+
+    upLoadTask.on(
+      "state_changed",
+      (snapshot) => {
+        console.log("snapshot", snapshot);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        upLoadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          setDescription((preMardown) => {
+            return preMardown + `![image](${downloadURL})`;
+          });
+        });
+      }
+    );
+  };
+    
+  const autoUploadImage = useMemo(() => {
+    return {
+      uploadImage: true,
+      imageUploadFunction,
+    };
+  }, []);
 
   const onChange = (value) => {
     setDescription(value);
@@ -10,7 +50,7 @@ const MarkdownEditor = ({ description, setDescription }) => {
 
   return (
     <div>
-      <SimpleMde value={description} onChange={onChange} />
+      <SimpleMde value={description} onChange={onChange} options={autoUploadImage} />
     </div>
   );
 };
